8274b7b24ff19a4f747ec0b424303ebbf856ccee
jane <jayndiritu36@gmail.com>
2025-11-09 14:54:27 +0300

    Addition

--- 9-add.js
+++ 9-add.js
@@ -5,9 +5,5 @@ function add(a, b) {
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
-if (isNaN(num1) || isNaN(num2)) {
-    console.log("Please provide two valid integers");
-} else {
-    const result = add(num1, num2);
-    console.log(result);
-}
+const result = add(num1, num2);
+console.log(result);
