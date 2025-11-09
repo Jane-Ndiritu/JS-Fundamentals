f722066dd5058561b335fa1e3b99692162db75a3
jane <jayndiritu36@gmail.com>
2025-11-09 14:26:14 +0300

    Multi line code

---
+++ 1-multi_languages.js
@@ -0,0 +1,8 @@
+myVar ={
+    firstLine: "Cis fun",
+    secondLine: "Python is cool",
+    thirdLine: "JavaScript is amazing"
+};
+console.log(myVar.firstLine);
+console.log(myVar.secondLine);
+console.log(myVar.thirdLine);
